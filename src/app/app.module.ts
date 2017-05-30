f7a3c2a235f4e50ab1db1516387781301fe246d9
Kevin Qu <kevinqu88@gmail.com>
2017-05-30 16:02:58 -0400

    app base structure and material design

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { MdButtonModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MdButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
